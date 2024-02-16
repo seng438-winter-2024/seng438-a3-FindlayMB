b541cac04df0aee196b522de354f3b2ad9242bb3
saraaah-qin <ya.qin@ucalgary.ca>
2024-02-15 23:30:17 -0700

    added ExpandTests class with complete test cases and added comments in AlGetterTests

--- jfreechart-1.0.19/tests/org/jfree/data/test/range/AllGetterTests.java
+++ jfreechart-1.0.19/tests/org/jfree/data/test/range/AllGetterTests.java
@@ -8,39 +8,58 @@ import org.jfree.data.Range;
 
 public class AllGetterTests {
     private Range range1;
-    private Range range3;
+    private Range range2;
     @BeforeClass public static void setUpBeforeClass() throws Exception {
     }
 
     @Before
     public void setUp() throws Exception {
         range1 = new Range(2, 9);
-        range3 = new Range(-2, 2);
-;
+        range2 = new Range(-2, 3.5);
+
     }
 
+    /**
+     * This test tests getLength function with inputs of a range
+     * from [2, 9]
+     * Expected outcome: returns a length of 7.
+     */
     @Test
-    public void getLengthShouldReturnLength() {
+    public void getLengthBasic() {
         assertEquals("Expected 7",
                 7, range1.getLength(),.000000001d);
     }
 
-    /*
-    *   This does not cover the illegalException thrown in the getLength function
-    *   because the object of Range lower > upper will not be created
-    */
+    /**
+     * This test tests getLength function with inputs of a range
+     * from [-2, 3.5]
+     * Expected outcome: returns a length of 5.5.
+     */
     @Test
-    public void getLengthShouldThrowException() {
-        try {
-            Range range2 = new Range(5, 3);
-            range2.getLength();
-            fail("Expected IllegalArgumentException was not thrown");
-        } catch (IllegalArgumentException e) {
-            String expectedMessage = "Range(double, double): require lower (" + 5.0 + ") <= upper (" + 3.0 + ").";
-            assertEquals(expectedMessage, e.getMessage());
-        }
+    public void getLengthNegativeAndPositive() {
+        assertEquals("Expected 5.5",
+                5.5, range2.getLength(),.000000001d);
     }
 
+    /**
+     * This test tests getLength function with inputs of a range
+     * from [5.0, 2.0]
+     * Expected outcome: throws IllegalArgumentException.
+     *
+     * This does not cover the illegalException thrown in the getLength function
+     * because the object of Range lower > upper will not be created
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void getLengthShouldThrowException() {
+        Range range = new Range(5.0, 2.0);
+        range.getLength();
+    }
+
+    /**
+     * This test tests getCentralValue function with inputs of a range
+     * from [2, 9]
+     * Expected outcome: returns a value of 5.5.
+     */
     @Test
     public void getCentralValueShouldReturn3_5() {
         assertEquals("Expected 3.5",
