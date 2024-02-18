e4628b1dfc94aeb24f63abc16454fbbd957ccaa9
Findlay Brown <78494998+FindlayMB@users.noreply.github.com>
2024-02-17 18:28:56 -0700

    Create ExpandTests.java

---
+++ jfreechart-1.0.19/tests/org/jfree/data/test/range/ExpandTests.java
@@ -0,0 +1,8 @@
+package org.jfree.data.test.range;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+import org.jfree.data.Range;
+public class ExpandTests {
+}
